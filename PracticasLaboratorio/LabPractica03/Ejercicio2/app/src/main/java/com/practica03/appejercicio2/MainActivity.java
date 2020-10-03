2e6ace1aec665d48a1d571c81b79b0f0c883deab
edwinsaavedra99 <40569878+edwinsaavedra99@users.noreply.github.com>
2020-10-03 12:31:25 -0500

    permisos
    El reproductor debe soolicitar permisos de lectura de archivos del celular

--- PracticasLaboratorio/LabPractica03/Ejercicio2/app/src/main/java/com/practica03/appejercicio2/MainActivity.java
+++ PracticasLaboratorio/LabPractica03/Ejercicio2/app/src/main/java/com/practica03/appejercicio2/MainActivity.java
@@ -1,14 +1,59 @@
 package com.practica03.appejercicio2;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final String TAG = "MainActivity";
+    private static final int PERMISSION_REQUEST_CODE_EXTERNAL_STORAGE = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        permits();
     }
+    public void permits(){
+        if (Build.VERSION.SDK_INT >= 23){
+            if(ContextCompat.checkSelfPermission(getApplicationContext(),
+                    Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED){
+                // Permisos concedidos
+                //LLAMAR METODO
+                Log.println(Log.INFO,TAG,"API >= 23");
+            } else{
+                if(shouldShowRequestPermissionRationale(Manifest.permission.READ_EXTERNAL_STORAGE)){
+                    // Mensaje de error
+                    Toast.makeText(getApplicationContext(), "External storage and camera permission required to read media", Toast.LENGTH_SHORT).show();
+                }
+                // Callback para solicitar permisos
+                requestPermissions(new String[] {Manifest.permission.READ_EXTERNAL_STORAGE}, PERMISSION_REQUEST_CODE_EXTERNAL_STORAGE);
+            }
+        } else {
+            //callCameraApp();
+            Log.println(Log.INFO,TAG,"API < 23");
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults){
+        if(requestCode == PERMISSION_REQUEST_CODE_EXTERNAL_STORAGE){
+            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(getApplicationContext(), "External read permission", Toast.LENGTH_SHORT).show();
+                //LLAMAR METODO
+            }else {
+                Toast.makeText(getApplicationContext(), "External read permission has not been granted, cannot open media", Toast.LENGTH_SHORT).show();
+            }
+        } else {
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+
+
 }
