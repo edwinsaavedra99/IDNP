b2821b8f78f6b66b18548287f5275b658d3fb8ac
edwinsaavedra99 <40569878+edwinsaavedra99@users.noreply.github.com>
2020-10-03 20:38:01 -0500

    mediaPlayer

--- PracticasLaboratorio/LabPractica03/Ejercicio2/app/src/main/java/com/practica03/appejercicio2/MainActivity.java
+++ PracticasLaboratorio/LabPractica03/Ejercicio2/app/src/main/java/com/practica03/appejercicio2/MainActivity.java
@@ -5,13 +5,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.SeekBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.File;
@@ -23,7 +31,16 @@ public class MainActivity extends AppCompatActivity {
     private static final int PERMISSION_REQUEST_CODE_EXTERNAL_STORAGE = 1;
     private ListView listMusic;
     private String [] items;
-    //private ArrayList<File> archivos;
+    private ImageView play,pause;
+    private TextView nameSong;
+    private SeekBar seekBar;
+    private static MediaPlayer mediaPlayer;
+    private int position;
+    //private ArrayList<File> mySongs;
+    private Thread updateseekbar;
+    private ArrayList<File> myMusic;
+    private String songName;
+    private boolean flagPause;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,9 +50,32 @@ public class MainActivity extends AppCompatActivity {
         permits();
 
     }
+
+    protected void onPause() {
+        super.onPause();
+        seekBar.setMax(mediaPlayer.getDuration());
+        if(mediaPlayer.isPlaying()){
+            pause.setBackgroundResource(R.drawable.ic_baseline_play_circle_filled_24);
+            mediaPlayer.pause();
+        }
+
+    }
+
+    protected void onRestart() {
+        super.onRestart();
+        if(!mediaPlayer.isPlaying() && flagPause == false){
+            pause.setBackgroundResource(R.drawable.ic_baseline_pause_circle_outline_24);
+            mediaPlayer.start();
+        }
+    }
+
     public void initialComponents(){
         this.listMusic = (ListView) findViewById(R.id.listMusic);
-        //this.archivos = new ArrayList<>();
+        this.play = findViewById(R.id.play);
+        this.pause = findViewById(R.id.pause);
+        this.nameSong = findViewById(R.id.nameSong);
+        this.seekBar = findViewById(R.id.barSong);
+        flagPause = false;
     }
 
     public ArrayList<File> findMusic(File file){
@@ -56,8 +96,73 @@ public class MainActivity extends AppCompatActivity {
         return arrayList;
     }
 
+    void playSongName(String songName, int position){
+        seekBar.setProgress(0);
+        updateseekbar = new Thread(){
+            @Override
+            public void run(){
+                int totalDuration = mediaPlayer.getDuration();
+                int currentPosition = 0;
+                while (currentPosition<totalDuration){
+                    try {
+                        sleep(500);
+                        currentPosition = mediaPlayer.getCurrentPosition();
+                        seekBar.setProgress(currentPosition);
+                    }catch (InterruptedException e){
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+        };
+
+        if(mediaPlayer!=null){
+            mediaPlayer.stop();
+            mediaPlayer.release();
+        }
+
+        this.nameSong.setText(songName);
+        this.nameSong.setSelected(true);
+        Uri uri = Uri.parse(myMusic.get(position).toString());
+        mediaPlayer=MediaPlayer.create(getApplicationContext(),uri);
+        mediaPlayer.start();
+        seekBar.setMax(mediaPlayer.getDuration());
+        //updateseekbar.start();
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                mediaPlayer.seekTo(seekBar.getProgress());
+            }
+        });
+
+        pause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                seekBar.setMax(mediaPlayer.getDuration());
+                if(mediaPlayer.isPlaying()){
+                    flagPause = true;
+                    pause.setBackgroundResource(R.drawable.ic_baseline_play_circle_filled_24);
+                    mediaPlayer.pause();
+                }else{
+                    pause.setBackgroundResource(R.drawable.ic_baseline_pause_circle_outline_24);
+                    mediaPlayer.start();
+                }
+            }
+        });
+    }
+
     void display(){
-        final ArrayList<File> myMusic = findMusic(Environment.getExternalStorageDirectory());
+        this.myMusic = findMusic(Environment.getExternalStorageDirectory());
         items = new String[myMusic.size()];
         System.out.println("display ::"+items.length);
         for( int i = 0 ; i < myMusic.size() ; i++){
@@ -65,6 +170,14 @@ public class MainActivity extends AppCompatActivity {
         }
         ArrayAdapter<String> arrayAdapter =  new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,items);
         listMusic.setAdapter(arrayAdapter);
+
+        listMusic.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                songName = listMusic.getItemAtPosition(position).toString();
+                playSongName(songName,position);
+            }
+        });
     }
 
 
