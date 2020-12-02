b23f4dee78caf1e73b2de2bf26d67fa3eb17a24a
edwinsaavedra99 <40569878+edwinsaavedra99@users.noreply.github.com>
2020-12-02 18:58:49 -0500

    fused

---
+++ PracticasLaboratorio/LabPractica09/app/src/main/java/com/lab09/question01/mapaslocation/Constants.java
@@ -0,0 +1,7 @@
+package com.lab09.question01.mapaslocation;
+
+public class Constants {
+    static final int LOCATION_SERVICES_ID = 175;
+    static final String ACTION_START_LOCATION_SERVICE = "startLocationService";
+    static final String ACTION_STOP_LOCATION_SERVICE = "stopLocationService";
+}
--- PracticasLaboratorio/LabPractica09/app/src/main/java/com/lab09/question01/mapaslocation/MapsActivity01.java
+++ PracticasLaboratorio/LabPractica09/app/src/main/java/com/lab09/question01/mapaslocation/MapsActivity01.java
@@ -1,17 +1,81 @@
 package com.lab09.question01.mapaslocation;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.app.ActivityManager;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Looper;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationAvailability;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
+import com.google.android.gms.location.SettingsClient;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 
 public class MapsActivity01 extends FragmentActivity implements OnMapReadyCallback {
+
+
+    //private final Handler handler = new Handler();
+    //private boolean isNetworkLocation, isGPSLocation;
+    /**
+     * FusedLocationProviderApi Save request parameters
+     */
+    //private LocationRequest mLocationRequest;
+
+
+    /**
+     * Provide callbacks for location events.
+     */
+    //private LocationCallback mLocationCallback;
+
+    /**
+     * An object representing the current location
+     */
+    //private Location mCurrentLocation;
+
+    //A client that handles connection / connection failures for Google locations
+    // (changed from play-services 11.0.0)
+    //private FusedLocationProviderClient mFusedLocationClient;
+
+    //private String provider;
 
     private GoogleMap mMap;
 
@@ -19,28 +83,90 @@ public class MapsActivity01 extends FragmentActivity implements OnMapReadyCallba
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps01);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
+        assert mapFragment != null;
         mapFragment.getMapAsync(this);
+        findViewById(R.id.startLocation).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkMyPermissionLocation();
+            }
+        });
+        findViewById(R.id.stopLocation).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stopLocationSerivce();
+            }
+        });
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+        LocationServices.mMap = mMap;
     }
+
+
+    private void checkMyPermissionLocation() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            //Permission Check
+            PermissionUtils.requestPermission(this);
+        } else {
+            //If you're authorized, start setting your location
+            //initGoogleMapLocation();
+            startLocationService();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        //If the request code does not match
+        if (requestCode != PermissionUtils.REQUEST_CODE) {
+            return;
+        }
+        if (PermissionUtils.isPermissionGranted(new String[]{
+                Manifest.permission.ACCESS_FINE_LOCATION}, grantResults)) {
+            //If you have permission, go to the code to get the location value
+            //initGoogleMapLocation();
+            startLocationService();
+        } else {
+            Toast.makeText(this, "Stop apps without permission to use location information", Toast.LENGTH_SHORT).show();
+            //finish();
+        }
+    }
+
+    /**
+     * Remove location information
+     */
+    @Override
+    public void onStop() {
+        super.onStop();
+        /*if (mFusedLocationClient != null) {
+            mFusedLocationClient.removeLocationUpdates(mLocationCallback);
+        }*/
+    }
+
+    private void startLocationService(){
+        if(!LocationServices.isMyServiceRunning){
+            Intent intent = new Intent(getApplicationContext(),LocationServices.class);
+            intent.setAction(Constants.ACTION_START_LOCATION_SERVICE);
+            startService(intent);
+            Toast.makeText(this,"Location service started",Toast.LENGTH_SHORT).show();
+        }
+    }
+    private void stopLocationSerivce(){
+        if(LocationServices.isMyServiceRunning){
+            Intent intent = new Intent(getApplicationContext(),LocationServices.class);
+            intent.setAction(Constants.ACTION_STOP_LOCATION_SERVICE);
+            startService(intent);
+            Toast.makeText(this,"Location service stopped",Toast.LENGTH_SHORT).show();
+        }
+    }
+
 }
