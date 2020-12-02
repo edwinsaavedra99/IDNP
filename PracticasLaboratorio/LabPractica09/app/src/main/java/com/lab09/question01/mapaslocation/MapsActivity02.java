00bfacf7a195d6ca5d493ffb4d4284449ca7f865
Luis Angel Lorenzo Quilla <louysotopo123@gmail.com>
2020-12-02 18:51:39 -0500

    lbs

--- PracticasLaboratorio/LabPractica09/app/src/main/java/com/lab09/question01/mapaslocation/MapsActivity02.java
+++ PracticasLaboratorio/LabPractica09/app/src/main/java/com/lab09/question01/mapaslocation/MapsActivity02.java
@@ -1,8 +1,12 @@
 package com.lab09.question01.mapaslocation;
 
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -10,19 +14,54 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.lab09.question01.mapaslocation.lbs.LocationTrack;
 
 public class MapsActivity02 extends FragmentActivity implements OnMapReadyCallback {
 
-    private GoogleMap mMap;
+
+    private static final int REQUEST_CODE_PERMISSION = 2;
+    String mPermission = Manifest.permission.ACCESS_FINE_LOCATION;
+    LocationTrack gps;
+    private Double latitude;
+    private Double longitude;
+    public static LatLng sydney;
+    public static GoogleMap mMap;
+    public static int count;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps02);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        latitude = -34d;
+        longitude= 150d;
+        count=0;
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        try {
+            if (ActivityCompat.checkSelfPermission(this, mPermission) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, new String[]{mPermission},
+                        REQUEST_CODE_PERMISSION);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        try{
+            gps = new LocationTrack(MapsActivity02.this);
+
+            if (gps.canGetLocation()) {
+                latitude = gps.getLatitude();
+                longitude = gps.getLongitude();
+                Toast.makeText(getApplicationContext(), "This is your Location : \nLatitude: " + latitude + "\nLongitude: " + longitude, Toast.LENGTH_SHORT).show();
+            }
+            else {
+                gps.showSettings();
+            }
+        }catch (Exception e){
+
+        }
     }
 
     /**
@@ -37,10 +76,18 @@ public class MapsActivity02 extends FragmentActivity implements OnMapReadyCallba
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        /*
+        sydney = new LatLng(latitude, longitude);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker lbs"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+*       */
+    }
+    public static void changeMarker(double latitude, double longitude){
+        sydney = new LatLng(latitude,longitude);
+        mMap.addMarker(new MarkerOptions().position(sydney).title(""+count));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        count++;
+
     }
 }
